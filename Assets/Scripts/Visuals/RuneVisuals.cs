a5f2fbe5feb3fa204462900ccd5eba454ba5b5e0
Daniel Åkesson <argilstudio@gmail.com>
2024-04-14 22:51:20 +0200

    changed to URP, added visual for shards

--- Assets/Scripts/Visuals/RuneVisuals.cs
+++ Assets/Scripts/Visuals/RuneVisuals.cs
@@ -11,6 +11,11 @@ public class RuneVisuals : Draggable
     public TextMeshProUGUI Description;
     public Image Icon;
 
+    public Material[] Edges;
+    public Material[] Surfaces;
+    public MeshRenderer EdgeRenderer;
+    public MeshRenderer SurfaceRenderer;
+
     private Rune rune;
     private Player player;
     public Rune Rune => rune;
@@ -25,6 +30,28 @@ public class RuneVisuals : Draggable
         Name.text = rune.Name;
         Description.text = rune.Text;
 
+        //Set material based on rarity
+        switch (rune.Rarity)
+        {
+            case Rarity.Starter:
+                EdgeRenderer.material = Edges[0];
+                SurfaceRenderer.material = Surfaces[0];
+                break;
+            case Rarity.Common:
+            case Rarity.None:
+                EdgeRenderer.material = Edges[1];
+                SurfaceRenderer.material = Surfaces[1];
+                break;
+            case Rarity.Rare:
+                EdgeRenderer.material = Edges[2];
+                SurfaceRenderer.material = Surfaces[2];
+                break;
+            case Rarity.Legendary:
+                EdgeRenderer.material = Edges[3];
+                SurfaceRenderer.material = Surfaces[3];
+                break;
+        }
+        //TODO: SET THE ICON
         UpdateStats();
     }
 
