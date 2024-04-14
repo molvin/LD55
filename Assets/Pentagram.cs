7bf02bb80521b8f04b3309403251b1b759854caf
molvin <akerstrom.perry@gmail.com>
2024-04-15 00:39:19 +0200

    artifacts fix

--- Assets/Pentagram.cs
+++ Assets/Pentagram.cs
@@ -5,34 +5,9 @@ using UnityEngine;
 public class Pentagram : MonoBehaviour
 {
     public GemSlot[] Slots;
-    private Dictionary<GemSlot, bool> isPlaying = new();
 
     private void Start()
     {
-        foreach(GemSlot slot in Slots)
-        {
-            isPlaying.Add(slot, false);
-        }
-    }
 
-    public void Cache()
-    {
-        foreach (GemSlot slot in Slots)
-        {
-            isPlaying[slot] = slot.ActiveParticles.isPlaying;
-        }
-    }
-    public void Pop()
-    {
-        foreach (GemSlot slot in Slots)
-        {
-            if (isPlaying.ContainsKey(slot))
-            {
-                if (isPlaying[slot])
-                    slot.ActiveParticles.Play();
-                else
-                    slot.ActiveParticles.Stop();
-            }
-        }
     }
 }
